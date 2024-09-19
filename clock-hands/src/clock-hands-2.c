a561f94c7d1b41f61d13cc42af0dabe3c6236f5b
Canis argentīnus 9999 <139458542+FourNinesFineK9@users.noreply.github.com>
2024-09-19 14:38:17 +0200

    Tighten boundary conditions

--- clock-hands/src/clock-hands-2.c
+++ clock-hands/src/clock-hands-2.c
@@ -3,7 +3,7 @@ Input
 delta_theta in degrees (+ or -) {0..360}
 
 Output
-List all times in "00:00:00.000" format that match going through the (12 - 1) intervals around the clock
+In "00:00:00.000" format going through the (12 - 1) intervals around the clock
 
 https://en.wikipedia.org/wiki/Clock_angle_problem
 
@@ -12,7 +12,6 @@ https://toytheater.com/clock/
 
 For hands alignment (require seconds):
 https://upload.wikimedia.org/wikipedia/commons/1/1a/Clock_angle_problem_graph.svg
-
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,30 +25,37 @@ int main(int argc, char *argv[])
   double delta_theta;
   int hour;
   double minute;
-  int tmp_hour;
-  double tmp_minute;
+  int thour;
+  double tminute;
 
   delta_theta = strtod(argv[1], NULL);
   delta_theta = fmod(delta_theta, 360);
+  if (delta_theta <= 0)
+    {
+    delta_theta = 360 + delta_theta;
+    }
   for (hour = 1; hour <= 11; hour++)
     {
-    minute = ( 2 * delta_theta + 60 * hour ) / 11;
+    minute = (60 * hour  - (2 * delta_theta) ) / 11;
     if (minute < 0)
       {
-      tmp_minute = 60 + minute;
-      tmp_hour = hour - 1;
+      tminute = 60 + minute;
+      thour = hour - 1;
       }
-    else if (minute >= 60)
-        {
-        tmp_minute = minute - 60;
-        tmp_hour = hour + 1;
-        }
-      else
-        {
-        tmp_minute = minute;
-        tmp_hour = hour;
-        }
-    printf ("%02d:%02.f:%06.3f\n", tmp_hour, floor(tmp_minute), (tmp_minute - floor(tmp_minute)) * 60.0);
+    else
+      {
+      tminute = minute;
+      thour = hour;
+      }
+    printf ("%02d:%02.f:%06.3f\n", thour, floor(tminute), (tminute - floor(tminute)) * 60.0);
+    }
+  if (delta_theta > 360/2)
+    {
+    printf ("%06.3f\n", delta_theta - 360.0);
+    }
+  else
+    {
+    printf ("%06.3f\n", delta_theta);
     }
   return 0;
   }
