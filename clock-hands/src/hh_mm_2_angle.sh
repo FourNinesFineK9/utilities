ee39eb4db45875f791daad946cf6fbd57963bb1e
Canis argentīnus 9999 <139458542+FourNinesFineK9@users.noreply.github.com>
2024-09-19 14:39:24 +0200

    Trivial but useful

---
+++ clock-hands/src/hh_mm_2_angle.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Input:
+# 10:10
+#
+# Time string in "hh:dd" format
+# 12 hour clock
+# No validation
+# Quick 'n dirty
+#
+H=`echo $1 | cut -d ":" -f1`  
+M=`echo $1 | cut -d ":" -f2`
+#
+units -t " 0.5 (60 $H + $M) - ($M * 6)" | xargs printf "# %-4.6f\n"
