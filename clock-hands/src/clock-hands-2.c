293da94b5b33f6740bc7f240b34e0363fb3430cb
Canis argentīnus 9999 <139458542+FourNinesFineK9@users.noreply.github.com>
2024-09-20 13:03:19 +0200

    Streamline aka rewrite

--- clock-hands/src/clock-hands-2.c
+++ clock-hands/src/clock-hands-2.c
@@ -7,55 +7,92 @@ In "00:00:00.000" format going through the (12 - 1) intervals around the clock
 
 https://en.wikipedia.org/wiki/Clock_angle_problem
 
+(delta_theta > 0), hour hand ahead
+(delta_theta == 0), conjunction
+(delta_theta < 0), minute hand ahead
+
 Interactive clock to experiment with
 https://toytheater.com/clock/
 
-For hands alignment (require seconds):
+To pick out patterns wtih granularity: seconds:
 https://upload.wikimedia.org/wikipedia/commons/1/1a/Clock_angle_problem_graph.svg
+
+Fencepost problem
+https://en.wikipedia.org/wiki/Off-by-one_error
+
+12 posts
+11 intervals
+
+Set
+first post (of the fence) to fence_post_first
+last post (of the fence) to fence_post_last
+
+By inspection of the results:
+intervals spaced by
+delta_time
+=
+Travel time around the 12 hour clock divided by 11
+
+Using GNU units and qalc:
+
+linux> units -t day
+24 hr = 86400 s
+linux> units -t "(1 day / 2) / 11"
+3927.2727 s
+linux> units -t "(1 day / 2) / 11" hms
+1;5;27.272727
+linux> units -t "1 hour + 5 minute + 27.272727 seconds"
+3927.2727 s
+delta_time = "01:05:27.27"
+linux> units -t "5 minute + 27.272727 seconds"
+327.27273 s
+linux> units -t "27.272727 seconds" minute
+0.45454545
+linux> units -t "1 hour + (60/11) minutes" hms
+1;5;27.272727
+linux> qalc "60/11"
+60 / 11 = 5 + 5/11 ≈ 5.454545455
+linux> qalc "(60/11 - 5 ) * 60"
+((60 / 11) − 5) × 60 = 300/11 = 27 + 3/11 ≈ 27.27272727
+
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-#define hour_hand (360.0/(12*60))
-#define minute_hand (360.0/60)
-
 int main(int argc, char *argv[])
   {
   double delta_theta;
+
   int hour;
   double minute;
-  int thour;
-  double tminute;
+  int i;
+  double j;
+
+  int fence_post_first = 1;
+  int fence_post_last = 12;
 
   delta_theta = strtod(argv[1], NULL);
   delta_theta = fmod(delta_theta, 360);
-  if (delta_theta <= 0)
+  for (i = fence_post_first; i < fence_post_last; i++)
     {
-    delta_theta = 360 + delta_theta;
-    }
-  for (hour = 1; hour <= 11; hour++)
-    {
-    minute = (60 * hour  - (2 * delta_theta) ) / 11;
-    if (minute < 0)
+    j = (60 * i  - (2 * delta_theta) ) / 11;
+    if (j < 0)
       {
-      tminute = 60 + minute;
-      thour = hour - 1;
+      hour = i - 1;
+      minute = 60 + j;
       }
-    else
+    else if (j >= 60)
       {
-      tminute = minute;
-      thour = hour;
+      hour = i + 1;
+      minute = j - 60;
       }
-    printf ("%02d:%02.f:%06.3f\n", thour, floor(tminute), (tminute - floor(tminute)) * 60.0);
-    }
-  if (delta_theta > 360/2)
-    {
-    printf ("%06.3f\n", delta_theta - 360.0);
-    }
-  else
-    {
-    printf ("%06.3f\n", delta_theta);
+      else
+        {
+        hour = i;
+        minute = j;
+        }
+    printf ("%02d:%02.f:%06.3f\n", hour, floor(minute), (minute - floor(minute)) * 60.0);
     }
   return 0;
   }
